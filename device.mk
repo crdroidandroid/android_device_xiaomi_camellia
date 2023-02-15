8b15ed93f6675918c4fe9798af8560116a3a2128
sarthakroy2002 <sarthakroy2002@gmail.com>
2023-02-15 23:54:23 +0530

    camellia: rro_overlays: Add Aperture QRCodeComponent support
    * Fixes QR QS tile in Vanilla Builds
    
    Co-authored-by: Michael Bestas <mkbestas@lineageos.org>
    Signed-off-by: Sarthak Roy <sarthakroy2002@gmail.com>
    Change-Id: Idd692578418f662ce6aa42ed10b227cd4816d9c9
    Signed-off-by: khaeruirgi <khaeruirgi24@gmail.com>

--- device.mk
+++ device.mk
@@ -374,6 +374,7 @@ PRODUCT_ENFORCE_RRO_TARGETS := *
 
 PRODUCT_PACKAGES += \
     ApertureOverlayCamellia \
+    ApertureQRScannerOverlayCamellia \
     CarrierConfigOverlayCamellia \
     FrameworksResOverlayCamellia \
     SettingsOverlayCamellia \
